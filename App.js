558e3576647e23e538fd7c1767b3f51ea99289c9
kaviyarasan <kaviyarasan@dorustree.in>
2022-07-05 11:36:21 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const App = express();
+const jwt = require("jsonwebtoken");
+const Port = 3002;
+App.use(express.json());
+const cors = require("cors");
+const fs = require('fs');
+const value = require("./db.json")
+
+App.listen(Port, () => {
+  console.log(`COOL Your App is Runnig GOOD ${Port}`);
+});
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+App.use(cors(corsOptions));
+App.get("/", (req, res) => {
+  res.send(value);
+});
+
+App.post("/checkAuth", (req,res)=>{
+ checkAuth(req,res)
+})
+
+App.post("/register",(req,res)=>{
+  const datas = value
+  datas.find(element => {
+    console.log("dbemail-->",element.email);
+
+  let valdata = datas?.find((data)=>{
+    return data.email === req.body.email
+  })  
+  console.log("valdata",valdata)
+if(valdata !== undefined){
+  res.status(400).json({
+    error:"User Already found"
+  });
+}else{
+  const data =datas.concat(req.body);
+  console.log("bodysEmail",req.body.email)
+  fs.writeFileSync('db.json',JSON.stringify(data));
+}
+  });
+})
+
+App.post("/login", (req, res) => {
+  const {email} = req.body
+  if (!req.body.email) {
+    res.status(400).json({
+      status: "falid to login user",
+      error: { msg: "User not found" },
+    });
+  }
+  const Token = jwt.sign({ email }, "hdhdhdhd", { expiresIn: "10m" });
+  const refreshToken = jwt.sign({ email }, "hdhdhdhd", { expiresIn: "1h" });
+  console.log(Token, "get login token");
+  res.status(200).json({
+    status:"success",
+    Token,
+    refreshToken,
+  });
+});
+//Refresh
+App.post("/refresh", (req, res) => {
+  console.log("refresh",req)
+  const refreshToken = req.headers["x-access-token"];
+  let decode = jwt.decode(refreshToken);
+  console.log("hai",decode)
+  let curentMail = decode?.email;
+  // let Mail = Data.find((user) =>user.Email);
+  console.log(curentMail, "curr");
+  if (curentMail) {
+    const Token = jwt.sign({ curentMail }, "hdhdhdhd", { expiresIn: "25m" });
+    return res.status(200).json({
+      status: {
+        Msg: "Your goto agin Sign In",
+        Token,
+        refreshToken,
+      },
+    });
+  }
+});
+
+const checkAuth = (req, res, next) => {
+  console.log("i am in check auth")
+  const  {TokenExpiredError } = jwt;
+  const catchError = (error, res) => {
+    if (error instanceof TokenExpiredError) {
+      return res
+        .status(401)
+        .send({ Message: "Unauthorized! AccessToken was expired!" });
+    }
+    return res.status(401).send({ message: "Unauthorized!" });
+  };
+  const token = req.headers["x-access-token"];
+  console.log("reeeeee",token)
+
+  if (!token) {
+    res.status(400).json({
+      errors: [{ msg: " Token is not Found" }]
+    });
+  }
+  jwt.verify(token, "hdhdhdhd", (error,decoded) => {
+    console.log("decoces",decoded)
+    if (error) {
+      return catchError(error, res);
+    }
+    // next();
+  });
+};
+
+//Get User
+
+
---
+++ Components/Data.js
@@ -0,0 +1,69 @@
+const Data=[]
+
+const Juice=[
+  {
+    "Name": "Stoberry",
+    "Price": "160/-",
+    "url": "https://4.imimg.com/data4/HE/AI/MY-7862486/strawberry-milkshake-500x500.jpg",
+    "id": 12
+  },
+  {
+    "Name": "Pineapple",
+    "Price": "60/-",
+    "url": "https://www.manjarokitchen.com/wp-content/uploads/rpress/2020/05/pineapple-juice.jpg",
+    "id": 8
+  },
+  {
+    "Name": "Watermelon",
+    "Price": "40/-",
+    "url": "https://mpng.subpng.com/20180706/aac/kisspng-watermelon-juice-fruit-lycopene-food-watermelon-juice-5b3f017bf1a328.5034279415308558039898.jpg",
+    "id": 9
+  },
+  {
+    "Name": "Orange",
+    "Price": "70/-",
+    "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv-wGcwyA994aYu_mIMOly10QSJYJR7JpTmQ&usqp=CAU",
+    "id": 3
+  },
+  {
+    "Name": "Mango Juice",
+    "Price": "85/-",
+    "url": "https://thumbs.dreamstime.com/z/mango-juice-splash-out-glass-fruit-isolated-white-background-203854688.jpg",
+    "id": 6
+  },
+  
+  
+  {
+    "Name": "Amla",
+    "Price": "30/-",
+    "url": "https://5.imimg.com/data5/CP/XE/MY-3074443/amla-juice-500x500.jpg",
+    "id": 10
+  },
+  
+  {
+    "Name": "Date",
+    "Price": "95/-",
+    "url": "https://farm6.staticflickr.com/5316/14408227525_4d6b477424_o.jpg",
+    "id": 13
+  },
+  {
+    "Name": "Dragan Juice",
+    "Price": "130/-",
+    "url": "https://static.libertyprim.com/files/familles/jus-de-pitaya-large.jpg?1571164071",
+    "id": 17
+  },
+  {
+    "Name": "Rose juse",
+    "Price": "80/-",
+    "url": "https://5.imimg.com/data5/BW/PD/NO/ANDROID-56506499/product-jpeg-500x500.jpg",
+    "id": 18
+  },
+  {
+    "Name": "Rose Apple",
+    "Price": "50/-",
+    "url": "https://img.onmanorama.com/content/dam/mm/en/food/kerala-kitchen/images/2018/4/24/champakka-neeru.jpg",
+    "id": 19
+  }
+      ]
+
+module.exports={Data,Juice}
